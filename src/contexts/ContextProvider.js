739fcc990d7ebe053c2f8e2a457ca5abb18a9a6f
Pinielm <pinielmakotose98@gmail.com>
2023-01-25 09:46:34 +0200

    first commit

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,64 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notifications: false,
+};
+
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+  const [currentMode, setCurrentMode] = useState("Light");
+  const [currentColor, setCurrentColor] = useState("#3C9D7");
+  const [themeSettings, setThemeSettings] = useState(false);
+
+  const setMode = (e) => {
+    console.log(e.target.value);
+    setCurrentMode(e.target.value);
+
+    localStorage.setItem("themeMode", e.target.value);
+
+    setThemeSettings(false);
+  };
+
+  const setColor = (color) => {
+    setCurrentColor(color);
+
+    localStorage.setItem("colorMode", color);
+
+    setThemeSettings(false);
+  };
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
+
+  return (
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+        currentColor,
+        currentMode,
+        themeSettings,
+        setThemeSettings,
+        setMode,
+        setColor,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
